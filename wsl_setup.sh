612c9bad7d6be4089ccf01fa003e60dbe45e3b20
Hayato Mizushima <hayato.m126@gmail.com>
2020-05-10 20:00:21 +0900

    feat: add firefox startup script

--- wsl_setup.sh
+++ wsl_setup.sh
@@ -1,9 +1,6 @@
 sudo apt update
 sudo apt upgrade
 
-# gui setting
-echo "export DISPLAY=192.168.18.2:0.0" | tee -a ~/.bashrc
-
 # japanese
 sudo apt -y install language-pack-ja
 sudo update-locale LANG=ja_JP.UTF8
@@ -12,4 +9,4 @@ sudo apt -y install manpages-ja manpages-ja-dev
 sudo apt -y install fonts-takao-pgothic
 
 # gui apps
-#sudo apt -y install firefox firefox-locale-ja
+sudo apt -y install firefox firefox-locale-ja
