b7ecbfd8261a04f39af1a059780b8c124f5b63ee
Hayato Mizushima <hayato.m126@gmail.com>
2020-05-10 20:11:11 +0900

    feat: input method

--- wsl_setup.sh
+++ wsl_setup.sh
@@ -1,12 +1,16 @@
 sudo apt update
 sudo apt upgrade
 
-# japanese
+# japanese environment
 sudo apt -y install language-pack-ja
 sudo update-locale LANG=ja_JP.UTF8
 sudo dpkg-reconfigure tzdata
 sudo apt -y install manpages-ja manpages-ja-dev
 sudo apt -y install fonts-takao-pgothic
+
+# input method
+sudo apt -y install fcitx-mozc dbus-x11 x11-xserver-utils
+dbus-uuidgen | sudo tee /var/lib/dbus/machine-id
 
 # gui apps
 sudo apt -y install firefox firefox-locale-ja
